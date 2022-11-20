707ac6eff57f19303375feabb2604be7849bd763
czadunayski <118408721+czadunayski@users.noreply.github.com>
2022-11-20 14:03:42 -0700

    Update cailey3.py
    revise USD to local currency in slider description

--- cailey3.py
+++ cailey3.py
@@ -251,7 +251,7 @@ elif country == 'Zimbabwe':
     currency = 'ZWL'
 
 #creating Input Monthly Payment Slider
-input_payment = st.slider('How much can you afford per month (USD)', 1.25, (rate*max(kiva_loans["repayment_per_mo"])), 1.0)
+input_payment = st.slider('How much can you afford per month (in your local currency)', 1.25, (rate*max(kiva_loans["repayment_per_mo"])), 1.0)
 
 #creating Prediction (button)
 if st.button('Make Prediction'):
